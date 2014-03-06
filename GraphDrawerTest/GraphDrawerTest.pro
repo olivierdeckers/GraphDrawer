bd1b7b98d81ff8eff46ace7e622c968858d4b5a5
olivier <deckers.olivier@gmail.com>
2014-03-06 12:09:32 +0100

    Implemented test for uniform grid initial energy

--- GraphDrawerTest/GraphDrawerTest.pro
+++ GraphDrawerTest/GraphDrawerTest.pro
@@ -6,9 +6,11 @@ CONFIG -= qt
 SOURCES += tsauniformgridtest.cpp
 
 unix {
-LIBS += -L$$PWD/../OGDF/_debug -lOGDF -lgtest -lpthread
-INCLUDEPATH += $$PWD/../OGDF
+LIBS += -L$$PWD/../../OGDF/_debug -lOGDF -lgtest -lpthread
+INCLUDEPATH += $$PWD/../../OGDF
 
 QMAKE_CXXFLAGS += -std=c++11
+QMAKE_LFLAGS += ../build-GraphDrawer-Desktop-Debug/tsauniformgrid.o \
+    ../build-GraphDrawer-Desktop-Debug/TSAPlanarity.o
 }
 
--- GraphDrawerTest/tsauniformgridtest.cpp
+++ GraphDrawerTest/tsauniformgridtest.cpp
@@ -1,7 +1,12 @@
 #include "gtest/gtest.h"
 
+#include "../GraphDrawer/ogdf/tsauniformgrid.h"
+#include "../GraphDrawer/ogdf/TSAPlanarity.h"
+
 #include <ogdf/basic/Graph.h>
 #include <ogdf/basic/GraphAttributes.h>
+#include <ogdf/fileformats/GmlParser.h>
+#include <ogdf/internal/energybased/UniformGrid.h>
 
 class TSAUniformGridTest : public ::testing::Test {
 protected:
@@ -10,23 +15,55 @@ protected:
     }
 
     virtual ~TSAUniformGridTest() {
-
+        delete m_G;
+        delete m_GA;
     }
 
     virtual void SetUp() {
+        m_G = new ogdf::Graph();
+        m_GA = new ogdf::GraphAttributes(*m_G, ogdf::GraphAttributes::nodeGraphics |
+           ogdf:: GraphAttributes::edgeGraphics );
 
+        ogdf::node a = m_G->newNode();
+        ogdf::node b = m_G->newNode();
+        ogdf::node c = m_G->newNode();
+        ogdf::node d = m_G->newNode();
+        m_G->newEdge(a, b);
+        m_G->newEdge(a, d);
+        m_G->newEdge(b,c);
+        m_G->newEdge(b,d);
+        m_G->newEdge(c,d);
+        m_G->newEdge(a, c);
+
+        setNodePos(a, 0, 0);
+        setNodePos(b, 0.7, 0.1);
+        setNodePos(c, 1, 1);
+        setNodePos(d, 0, 1);
     }
 
     virtual void TearDown() {
 
     }
 
-private:
-    ogdf::Graph* G;
-    ogdf::GraphAttributes* GA;
+    ogdf::Graph* m_G;
+    ogdf::GraphAttributes* m_GA;
+
+    void setNodePos(ogdf::node n, double x, double y) {
+        m_GA->x(n) = x;
+        m_GA->y(n) = y;
+    }
 };
 
-TEST_F(TSAUniformGridTest, GridCorrectness) {
+TEST_F(TSAUniformGridTest, InitialGridCorrectness) {
+    ogdf::TSAUniformGrid grid = ogdf::TSAUniformGrid(*m_GA);
+
+    ogdf::TSAPlanarity *planarity = new ogdf::TSAPlanarity(*m_GA);
+    planarity->computeEnergy();
+
+    ogdf::UniformGrid originalGrid = ogdf::UniformGrid(*m_GA);
+    EXPECT_EQ(1, planarity->energy()) << "Planarity correctly detects one crossing";
+    EXPECT_EQ(1, grid.numberOfCrossings()) << "Grid correctly detects one crossing";
+    EXPECT_EQ(1, originalGrid.numberOfCrossings()) << "Original Grid correctly detects one crossing";
 
 }
 
