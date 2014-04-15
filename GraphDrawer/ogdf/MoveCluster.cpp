fe416c24a26c7d9ecdb968d08273ea2913fa65cb
olivier <deckers.olivier@gmail.com>
2014-04-15 10:52:55 +0200

    Small changes to clustering algorithm: handle lowest degree nodes first and keep track of all nodes adjacent to the current cluster

--- GraphDrawer/ogdf/MoveCluster.cpp
+++ GraphDrawer/ogdf/MoveCluster.cpp
@@ -2,16 +2,24 @@
 
 namespace ogdf {
 
+class NodeDegreeComparer {
+public:
+    bool less(node a, node b) const
+    {
+        return a->degree() < b->degree();
+    }
+};
+
 MoveCluster::MoveCluster(ogdf::GraphAttributes &GA) : NeighbourhoodStructure(GA)
 {
     List<node> nodes;
     m_G.allNodes(nodes);
+    nodes.quicksort(NodeDegreeComparer());
     int clusterIdx = 0;
 
     while(!nodes.empty())
     {
-        int r = randomNumber(0, nodes.size() - 1);
-        ListIterator<node> n = nodes.get(r);
+        ListIterator<node> n = nodes.get(0);
 
         Cluster c;
         growCluster(*n, c, nodes, clusterIdx);
@@ -27,8 +35,10 @@ void MoveCluster::growCluster(node start, Cluster &c, List<node> &nodes, int clu
 {
     c.nodes.pushBack(start);
     m_GA.labelNode(start) = String(std::to_string(clusterIdx).data());
+    m_GA.labelNode(start) += "s";
     nodes.del(nodes.get(nodes.search(start)));
 
+    List<node> adjacentNodes; // List of nodes adjacent to a node in the cluster
     bool clusterExpanded = true;
     node lastNodeAdded = start;
 
@@ -36,25 +46,31 @@ void MoveCluster::growCluster(node start, Cluster &c, List<node> &nodes, int clu
     {
         clusterExpanded = false;
 
+        adjEntry adj;
+        forall_adj(adj, lastNodeAdded) if(nodes.search(adj->twinNode()) != -1 && adjacentNodes.search(adj->twinNode()) == -1)
+        {
+            adjacentNodes.pushBack(adj->twinNode());
+        }
+
         node bestCandidate;
         double bestScore = 0;
 
-        adjEntry adj;
-        forall_adj(adj, lastNodeAdded) if(nodes.search(adj->twinNode()) != -1)
+        ListIterator<node> it = adjacentNodes.begin();
+        for(; it.valid(); it++)
         {
-            node candidate = adj->twinNode();
+            node candidate = *it;
             double score = 0.0;
 
-            ListIterator<node> it;
-            for(it = c.nodes.begin(); it.valid(); it++)
+            ListIterator<node> it2;
+            for(it2 = c.nodes.begin(); it2.valid(); it2++)
             {
-                if(m_G.searchEdge(candidate, *it) != 0) {
+                if(m_G.searchEdge(candidate, *it2) != 0) {
                     score++;
                 }
             }
-            score /= c.nodes.size();
+            score /= candidate->degree();
 
-            if(score > 0.5 && score > bestScore) {
+            if( (score >= 0.3 && score > bestScore) || (score == bestScore && candidate->degree() < bestCandidate->degree()) ) {
                 bestScore = score;
                 bestCandidate = candidate;
             }
@@ -70,6 +86,10 @@ void MoveCluster::growCluster(node start, Cluster &c, List<node> &nodes, int clu
             int nodeIdx = nodes.search(bestCandidate);
             OGDF_ASSERT(nodeIdx != -1);
             nodes.del(nodes.get(nodeIdx));
+
+            nodeIdx = adjacentNodes.search(bestCandidate);
+            OGDF_ASSERT(nodeIdx != -1);
+            adjacentNodes.del(adjacentNodes.get(nodeIdx));
         }
     }
 
